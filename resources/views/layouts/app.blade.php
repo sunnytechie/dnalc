d4e3a620a73df145462b1e5753d9f7482b294e99
sunnytechie <sunnyict001@gmail.com>
2024-04-22 11:14:02 +0100

    Tawk

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,5 +29,19 @@
 
 
         @yield('scripts')
+
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+            (function () {
+                var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/63bfb9dd47425128790d01dd/1gmies2ns';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin', '*');
+                s0.parentNode.insertBefore(s1, s0);
+            })();
+        </script>
+        <!--End of Tawk.to Script-->
     </body>
 </html>
