3ef0d3f6ef84076dc5ac29c97c3defd0eb4e6d4d
sunnytechie <sunnyict001@gmail.com>
2024-06-27 15:28:52 +0100

    flyer width size

--- resources/views/snippets/main/slide.blade.php
+++ resources/views/snippets/main/slide.blade.php
@@ -7,7 +7,7 @@
     <div class="carousel-inner" style="height: 100vh">
         @foreach ($sliders as $slider)
         <div class="carousel-item @if($loop->first) active @endif">
-            <img style="height: 100vh; width: 100%; object-fit: cover;" src="{{ asset('storage/' . $slider->flyer) }}" class="d-block w-100" alt="...">
+            <img style="height: 100vh; width: 100%; object-fit: contain;" src="{{ asset('storage/' . $slider->flyer) }}" class="d-block w-100" alt="...">
             <div class="carousel-caption d-none d-md-block">
             <h5 class="text-white">{{ $slider->title }}</h5>
             <div class="text-white">{!! $slider->description !!}</div>
